a6ca119e87a802ed48e062715724e4bdedf819b2
NghiaNDabc <203nghiapham@gmail.com>
2024-11-13 00:46:15 +0700

    config axios

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -10,7 +10,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        const accessToken = Cookies.get('accessToken');
+        const accessToken = localStorage.getItem('accessToken');
         if (accessToken) {
             config.headers['Authorization'] = `Bearer ${accessToken}`;
         }
@@ -23,21 +23,26 @@ axiosInstance.interceptors.response.use(
     (response) => response,
     async (error) => {
         const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
+        if (error.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
             try {
-                const refreshResponse = await axiosInstance.post('v1/Auth/refresh-token', {
-                    refreshRoken: Cookies.get('refreshToken'),
-                });
-                const { accessToken } = refreshResponse.data;
-
-                Cookies.set('accessToken', accessToken, { expires: 1 });
+                const refreshToken = localStorage.getItem('refreshToken');
+                const refreshResponse = await axiosInstance.post('/v1/Auth/refresh-token', refreshToken);
+                const accessToken = refreshResponse.data.data.token;
+                localStorage.setItem('accessToken', accessToken);
                 axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
                 return axiosInstance(originalRequest);
             } catch (refreshError) {
-                // Nếu refresh token cũng hết hạn, yêu cầu người dùng đăng nhập lại
-                window.location.href = '/login';
+                // Nếu refresh token cũng hết hạn, yêu cầu người dùng đăng nhập lại\
+                console.log('Lỗi');
+
+                console.error(refreshError);
+                window.location.href = '/admin';
             }
+        }
+        if (error.response?.status === 500 || !error.response) {
+            console.error('Server error or network connection issue:', error);
+            window.location.href = '/error'; // Redirect to a default error page
         }
         return Promise.reject(error);
     },
